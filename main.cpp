47d6572c2782f719b6979b35b79bce4cf396c5ef
Evgeny Cherkashin <eugeneai@irnok.net>
2022-05-21 10:13:11 +0800

    Added Makefile to run on UNIX

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@ using namespace std;
 int main()
 {
     FRIS f;
+    cout << "Loading data\n";
     f.loadData("./data3.txt");
+    cout << "Frame loaded\n";
     f.printFrame(cout);
+    cout << "Calculating\n";
     f.calculate();
+    cout << "Print result\n";
     f.outResult(cout);
 
     return 0;
